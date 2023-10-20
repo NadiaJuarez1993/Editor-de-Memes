1e105c9246cd7571d7ebe468c3b086437eb85d03
Nadia Juarez <nadiajuarezz@hotmail.com>
2023-10-20 19:00:25 -0300

    adding js filters

--- JS/main.js
+++ JS/main.js
@@ -19,3 +19,48 @@ const $ = (selector) => document.querySelector(selector)
 //EVENTOS
 
 //Filtros
+//brillo
+$("#brightness") . addEventListener("input", (e) => {
+    $(".image-container").style.filter = `brightness(${e.target.value})`
+})
+
+//opacidad
+$("#opacity") . addEventListener("input", (e) => {
+    $(".image-container").style.filter = `opacity(${e.target.value})`
+})
+
+//Contraste
+$("#contrast") . addEventListener("input", (e) => {
+    $(".image-container").style.filter = `contrast(${e.target.value}%)`
+})
+
+//blur
+$("#blur") . addEventListener("input", (e) => {
+    $(".image-container").style.filter = `blur(${e.target.value}px)`
+})
+
+//escala de grises
+$("#grayscale") . addEventListener("input", (e) => {
+    $(".image-container").style.filter = `grayscale(${e.target.value}%)`
+})
+
+//sepia
+$("#sepia") . addEventListener("input", (e) => {
+    $(".image-container").style.filter = `sepia(${e.target.value}%)`
+})
+
+//hue
+$("#hue-rotation") . addEventListener("input", (e) => {
+    $(".image-container").style.filter = `hue-rotation(${e.target.value}deg)`
+})
+
+//saturacion
+$("#saturation") . addEventListener("input", (e) => {
+    $(".image-container").style.filter = `saturation(${e.target.value}%)`
+})
+
+//negativo
+$("#invert") . addEventListener("input", (e) => {
+    $(".image-container").style.filter = `invert(${e.target.value})`
+})
+

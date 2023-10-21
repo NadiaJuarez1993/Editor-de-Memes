a96aa5671e1a00b776051a71f3e065b29ee21460
Nadia Juarez <nadiajuarezz@hotmail.com>
2023-10-20 21:23:13 -0300

    adding button reset filters

--- JS/main.js
+++ JS/main.js
@@ -10,8 +10,11 @@ const $ = (selector) => document.querySelector(selector)
 
 //filter
 const allFiltersMeme = () =>{
-    $(".image-container").style.filter = `brightness(${$("#brightness")}.value ) opacity(${$("#opacity")}.value) contrast(${$("#contrast")}.value %) blur(${$("blur")}.value px)  grayscale(${$("#grayscale")}.value %)  sepia(${$("#sepia")}.value %) hue-rotation(${$("#hue-rotation")}.value deg) saturation(${$("#saturation")}%) invert(${$("#invert")}) `
+    $(".image-container").style.filter = `brightness(${$("#brightness")}.value 1) opacity(${$("#opacity")}.value 1) contrast(${$("#contrast")}.value %) blur(${$("blur")}.value px)  grayscale(${$("#grayscale")}.value %)  sepia(${$("#sepia")}.value %) hue-rotation(${$("#hue-rotation")}.value deg) saturation(${$("#saturation")}.value %) invert(${$("#invert")}.value 1) `
 }
+
+//reset button
+
 
 
 
@@ -29,29 +32,43 @@ $("#url-image") .addEventListener("input", (e) => {
 
 //Filter
 //brillo
-$("#brightness") . addEventListener("input", allFiltersMeme)
+$("#brightness").addEventListener("input", allFiltersMeme)
 
 //opacidad
-$("#opacity") . addEventListener("input", allFiltersMeme)
+$("#opacity").addEventListener("input", allFiltersMeme)
 
 //Contraste
-$("#contrast") . addEventListener("input", allFiltersMeme)
+$("#contrast").addEventListener("input", allFiltersMeme)
 
 //blur
-$("#blur") . addEventListener("input", allFiltersMeme)
+$("#blur").addEventListener("input", allFiltersMeme)
 
 //escala de grises
-$("#grayscale") . addEventListener("input", allFiltersMeme)
+$("#grayscale").addEventListener("input", allFiltersMeme)
 
 //sepia
-$("#sepia") . addEventListener("input", allFiltersMeme)
+$("#sepia").addEventListener("input", allFiltersMeme)
 
 //hue
-$("#hue-rotation") . addEventListener("input", allFiltersMeme)
+$("#hue-rotation").addEventListener("input", allFiltersMeme)
 
 //saturacion
-$("#saturation") . addEventListener("input", allFiltersMeme)
+$("#saturation").addEventListener("input", allFiltersMeme)
 
 //negativo
-$("#invert") . addEventListener("input", allFiltersMeme)
+$("#invert").addEventListener("input", allFiltersMeme)
 
+//reset button
+$("#reset").addEventListener("click", () => {
+    $("#brightness").value = "1"
+    $("#opacity").value = "1"
+    $("#contrast").value = "100%"
+    $("#blur").value = "0px"
+    $("#grayscale").value = "0%"
+    $("#sepia").value = "0%"
+    $("#hue-rotation").value = "0deg"
+    $("#saturation").value = "100%"
+    $("#invert").value = "1"
+    $(".url-image-container").stile.filter = "none"
+
+})
